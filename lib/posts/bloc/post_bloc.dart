cfebaaaa8ea120ccd1d966dca5e7b8b99e42bee6
Ramadhan <ramadanazam93@gmail.com>
2024-05-28 16:52:22 +0800

    wip

---
+++ lib/posts/bloc/post_bloc.dart
@@ -0,0 +1,7 @@
+import 'package:bloc_infinite_list/posts/models/post.dart';
+import 'package:equatable/equatable.dart';
+
+part 'post_event.dart';
+part 'post_state.dart';
+
+class PostBloc extends Bloc<PostEvent, PostState> {}
