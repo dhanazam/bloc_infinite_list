ed862de5857215597ffbb55f4ff09c7d04debd32
Ramadhan <ramadanazam93@gmail.com>
2024-05-29 23:39:13 +0800

    add debounce

--- lib/posts/bloc/post_bloc.dart
+++ lib/posts/bloc/post_bloc.dart
@@ -1,47 +1,52 @@
 import 'dart:async';
 
+import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:bloc_infinite_list/posts/models/post.dart';
 import 'package:equatable/equatable.dart';
 import 'package:bloc/bloc.dart';
 import 'package:http/http.dart' as http;
+import 'package:stream_transform/stream_transform.dart';
 
 part 'post_event.dart';
 part 'post_state.dart';
+
+const throttleDuration = Duration(milliseconds: 100);
+
+EventTransformer<E> throttleDroppable<E>(Duration duration) {
+  return (events, mapper) {
+    return droppable<E>().call(events.throttle(duration), mapper);
+  };
+}
 
 class PostBloc extends Bloc<PostEvent, PostState> {
   PostBloc({required this.httpClient}) : super(const PostState()) {
     on<PostFetched>(
       _onPostFetched,
-      transformer: throttleDroppable(),
+      transformer: throttleDroppable(throttleDuration),
     );
   }
 
   final http.Client httpClient;
 
-  Future<void> _onPostFetched(PostFetched event, Emitter<PostState> emit) async {
+  Future<void> _onPostFetched(
+      PostFetched event, Emitter<PostState> emit) async {
     if (state.hasReachedMax) return;
     try {
       if (state.status == PostStatus.initial) {
         final posts = await _fetchPosts();
         return emit(state.copyWith(
-          status: PostStatus.success,
-          posts: posts,
-          hasReachedMax: false
-        ));
+            status: PostStatus.success, posts: posts, hasReachedMax: false));
       }
       final posts = await _fetchPosts(state.posts.length);
       emit(posts.isEmpty
-        ? state.copyWith(hasReachedMax: true)
-        : state.copyWith(
-            status: PostStatus.success,
-            posts: List.of(state.posts)..addAll(posts),
-            hasReachedMax: false,
-        ));
+          ? state.copyWith(hasReachedMax: true)
+          : state.copyWith(
+              status: PostStatus.success,
+              posts: List.of(state.posts)..addAll(posts),
+              hasReachedMax: false,
+            ));
     } catch (_) {
       emit(state.copyWith(status: PostStatus.failure));
     }
   }
-}
-
-throttleDroppable() {
 }
