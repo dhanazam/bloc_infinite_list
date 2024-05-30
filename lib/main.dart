d74f6b490ed6d87cffb5f8f3153988c4b2e5fd20
Ramadhan <ramadanazam93@gmail.com>
2024-05-30 11:51:08 +0800

    add root widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:bloc_infinite_list/simple_bloc_observer.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  Bloc.observer = const SimpleBlocObserver();
+  runApp(const App());
 }
